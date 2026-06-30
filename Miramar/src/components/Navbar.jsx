96bb6ad427d16342289e0a9067d783596f6003b8
VTheGreek <vasilis.proefth@gmail.com>
2026-07-01 00:13:41 +0300

    Feat: Created a variable and used the map method to identify the route type objects

--- Miramar/src/components/Navbar.jsx
+++ Miramar/src/components/Navbar.jsx
@@ -1,6 +1,40 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
+    const navLinks = [
+        {
+            label: 'home',
+            to: '/',
+            type: 'route'
+        },
+        {
+            label: 'gallery',
+            to: '#gallery',
+            type: 'scroll'
+        },
+        {
+            label: 'menu',
+            to: '/menu',
+            type: 'route'
+        },
+        {
+            label: 'contact',
+            to: '#contact',
+            type: 'scroll'
+        }
+    ]
+
+    const links = navLinks.map(link => {
+        if (link.type === 'route') {
+            return <Link to={link.to}>
+                        {link.label}
+                    </Link>
+        }
+    })
     return(
-        <nav></nav>
+        <nav>
+            {links}
+        </nav>
     )
 }
 
